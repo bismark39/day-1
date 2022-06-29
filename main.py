820ecddf6b1bd2dc6d42312a1c5c6485b7c0d47d
Bismark Frimpong <53122436+bismark39@users.noreply.github.com>
2022-06-29 13:25:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+greeting=print("Welcome to Biz name generator")
+city=input("Which city did you grew in?\n ")
+pet_name=input("What is the name of your pet?\n ")
+print("Your band name could be " + city + " "+ pet_name)
